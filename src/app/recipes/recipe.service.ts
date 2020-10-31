f133da148346fcbb04866fd9410f45a4cfe724c1
Ulaş Şeker <ulass.seker@gmail.com>
2020-10-31 15:33:26 +0300

    passing ingredients from recipes to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,38 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/spaghetti-puttanesca_1-1ce4e81.jpg?quality=90&resize=440%2C400'),
-        new Recipe('A Test Recipe 2', 'This is another recipe', 'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg')
+        new Recipe(
+            'A Test Recipe', 
+            'This is a recipe', 
+            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/spaghetti-puttanesca_1-1ce4e81.jpg?quality=90&resize=440%2C400', 
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'A Test Recipe 2', 
+            'This is another recipe', 
+            'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg', 
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
       ];
+
+    constructor(private shoppingListService : ShoppingListService){}
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients : Ingredient[]){
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
